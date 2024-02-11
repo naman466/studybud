0bdb42852a06bf23a8db0b6bb60977799fc4d7a8
naman466 <namantyagi466@gmail.com>
2024-02-11 17:22:25 +0530

    Added user profile page.

--- base/urls.py
+++ base/urls.py
@@ -9,6 +9,7 @@ urlpatterns =[
 
     path('', views.home, name = "home"),
     path('room/<int:pk>/', views.room, name = "room"),
+    path('profile/<str:pk>', views.userprofile, name = "user-profile"),
     path('create-room/',views.createroom, name = "create-room"),
     path('update-room/<int:pk>/',views.updateroom, name = "update-room"),
     path('delete-room/<int:pk>/',views.deleteroom, name = "delete-room"),
